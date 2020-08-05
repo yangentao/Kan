7e4e00ac745b495cf8cf95debd6a56fccda8d75e
Entao <entaoyang@163.com>
2020-08-06 00:08:09 +0800

    2

--- qrx/src/main/java/dev/entao/kan/qrx/CameraXView.kt
+++ qrx/src/main/java/dev/entao/kan/qrx/CameraXView.kt
@@ -1,69 +1,113 @@
 package dev.entao.kan.qrx
 
 import android.content.Context
-import android.graphics.Matrix
-import android.util.Rational
+import android.hardware.display.DisplayManager
 import android.util.Size
-import android.view.Surface
-import android.view.TextureView
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
-import dev.entao.kan.appbase.TaskQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraXView(context: Context) : PreviewView(context) {
-    private val taskHandler: TaskQueue = TaskQueue("image_analysis")
-    var preview: Preview? = null
     var resolution = Size(1280, 720)
     var onReady: () -> Unit = {}
     var onResult: (String) -> Unit = {}
+    var aspectRatio: Int = AspectRatio.RATIO_16_9
+
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var cameraInst: Camera? = null
+
+    private var exeService: ExecutorService? = null
+    private val displayManager by lazy {
+
+        context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayAdded(displayId: Int) = Unit
+        override fun onDisplayRemoved(displayId: Int) = Unit
+        override fun onDisplayChanged(displayId: Int) {
+            imageCapture?.targetRotation = this@CameraXView.display.rotation
+            imageAnalyzer?.targetRotation = this@CameraXView.display.rotation
+        }
+    }
+
+    @Suppress("unused")
+    fun stop() {
+        this.imageCapture = null
+        this.imageAnalyzer = null
+        this.cameraInst = null
+        exeService?.shutdown()
+        exeService = null
+        displayManager.unregisterDisplayListener(displayListener)
+    }
 
     fun start(lifeOwner: LifecycleOwner) {
-        val ratio = Rational(9, 16)
-        val rotation = this.display.rotation
+        displayManager.registerDisplayListener(displayListener, null)
 
-
-        val cpf = ProcessCameraProvider.getInstance(context)
-
-        cpf.addListener(Runnable {
-            val cp = cpf.get()
-            cp.unbindAll()
-            val pre = Preview.Builder().apply {
-                setTargetAspectRatio(AspectRatio.RATIO_16_9)
-                setTargetRotation(rotation)
-                setTargetResolution(resolution)
-            }.build()
-            val ex = ContextCompat.getMainExecutor(context)
-
-            val cs = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-            val qrAnaly = QRImageAnalysis()
-            qrAnaly.onResult = {
-                invokeResult(it)
-            }
-            val imgAnalyzer = ImageAnalysis.Builder().build().also {
-                it.setAnalyzer(ex, qrAnaly)
-            }
-
-            val imgCap = ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build()
-            val camera = cp.bindToLifecycle(lifeOwner, cs, pre, imgCap, imgAnalyzer)
-            pre.setSurfaceProvider(this.createSurfaceProvider())
-//            pre.setSurfaceProvider(this.createSurfaceProvider(camera.cameraInfo))
-
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            cameraProvider.unbindAll()
+            doStart(lifeOwner, cameraProvider)
             onReady()
         }, ContextCompat.getMainExecutor(context))
+    }
 
+    private fun doStart(lifeOwner: LifecycleOwner, cameraProvider: ProcessCameraProvider) {
+        val rotation = this.display.rotation
+        val lens = when {
+            cameraProvider.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) -> {
+                CameraSelector.LENS_FACING_BACK
+            }
+            cameraProvider.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) -> {
+                CameraSelector.LENS_FACING_FRONT
+            }
+            else -> {
+                return
+            }
+        }
+        val cameraExec: ExecutorService = Executors.newSingleThreadExecutor()
+        exeService = cameraExec
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lens).build()
+
+        val pre = Preview.Builder().apply {
+            setTargetAspectRatio(aspectRatio)
+            setTargetRotation(rotation)
+            setTargetResolution(resolution)
+        }.build()
+
+
+        val qrAnaly = QRImageAnalysis()
+        qrAnaly.onResult = {
+            invokeResult(it)
+        }
+        val imgAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(aspectRatio)
+            .setTargetRotation(rotation)
+            .build().also {
+                it.setAnalyzer(cameraExec, qrAnaly)
+            }
+
+        val imageCapture = ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build()
+        val camera = cameraProvider.bindToLifecycle(lifeOwner, cameraSelector, pre, imageCapture, imgAnalyzer)
+        pre.setSurfaceProvider(this.createSurfaceProvider())
+        this.imageCapture = imageCapture
+        this.imageAnalyzer = imgAnalyzer
+        this.cameraInst = camera
 
     }
 
     fun isTorchOn(): Boolean {
-//        return this.preview?.isTorchOn ?: false
-        return false
+        return this.cameraInst?.cameraInfo?.torchState?.value == TorchState.ON
     }
 
     fun setTorchOn(on: Boolean) {
-//        this.preview?.enableTorch(on)
+        this.cameraInst?.cameraControl?.enableTorch(on)
     }
 
     private fun invokeResult(s: String) {
